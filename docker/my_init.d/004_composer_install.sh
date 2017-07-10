cf0e35af2ce28257a863e6dbf446140c9d8062c4
Joshua Wilson <jwilson@waystone.systems>
2017-07-10 17:19:18 -0400

    move setting shim

--- docker/my_init.d/004_composer_install.sh
+++ docker/my_init.d/004_composer_install.sh
@@ -9,3 +9,6 @@ chown www-data:www-data /.composer
 
 #run composer install
 su -s "/bin/bash" -c "cd /app/ && composer install" www-data
+
+chmod 660 /app/web/sites/default/settings.php 2> /dev/null
+echo "<?php require(dirname(\$app_root) . '/settings.php');" > /app/web/sites/default/settings.php
