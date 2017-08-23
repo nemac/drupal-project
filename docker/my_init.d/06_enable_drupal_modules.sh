c166b719f4cf6168b4cd6c83e7d9e53228e2f3c4
Joshua Wilson <jwilson@waystone.systems>
2017-08-23 11:15:15 -0400

    enable modules after drupal is ready

--- docker/my_init.d/06_enable_drupal_modules.sh
+++ docker/my_init.d/06_enable_drupal_modules.sh
@@ -3,13 +3,20 @@
 #
 # This script is run when the container is first started.
 #
-# Checks if there are tables in the database, if not attempts to restore the most recent committed dump.
+# Wait for Drupal to bootstrap successfully (connect to database, files in place, etc),
+# then attempt to activate features module and s3fs
 #
 
 source /etc/container_environment.sh
 
-drush pm-enable features features_ui -y
+{
+  while [[ "$(drush core-status | grep 'Drupal bootstrap[ ]*:[ ]*Successful')" == '' ]]; do
+    sleep 30
+  done
+  drush pm-enable features features_ui -y
 
-if [[ ! -z "${ASSET_STORE}" ]]; then
-drush pm-enable s3fs -y
-fi
+  if [[ ! -z "${ASSET_STORE}" ]]; then
+    drush pm-enable s3fs -y
+  fi
+
+} &
